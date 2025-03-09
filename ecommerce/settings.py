7154c6c49fe26b408a51ee303a410a5f49007d3d
JWCurtis94 <jameswcurtis94@outlook.com>
2025-03-09 15:24:09 +0000

    Update settings.py with local PostgreSQL settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -67,11 +67,11 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ.get('DB_NAME'),
-            'USER': os.environ.get('DB_USER'),
-            'PASSWORD': os.environ.get('DB_PASSWORD'),
-            'HOST': os.environ.get('DB_HOST'),
-            'PORT': os.environ.get('DB_PORT'),
+            'NAME': os.environ.get('DB_NAME', 'your_db_name'),
+            'USER': os.environ.get('DB_USER', 'postgres'),
+            'PASSWORD': os.environ.get('DB_PASSWORD', 'Cavaliers16!'),
+            'HOST': os.environ.get('DB_HOST', 'localhost'),
+            'PORT': os.environ.get('DB_PORT', '5432'),
         }
     }
 
