0172c36fdb4b3803469adc87c606cffaf4416ee4
JWCurtis94 <jameswcurtis94@outlook.com>
2025-03-13 19:43:27 +0000

    Configure database for Heroku

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -60,13 +60,19 @@ MIDDLEWARE = [
     'allauth.account.middleware.AccountMiddleware',
 ]
 
-# Database configuration - simplified to always use dj_database_url
-# This will use DATABASE_URL from environment (provided by Heroku PostgreSQL add-on)
+# Database configuration
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
+# Configure for Heroku Postgres
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 TEMPLATES = [
     {
